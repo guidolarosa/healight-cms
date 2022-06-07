b956cc845a431dcf0153bbf0aa4bba35674d5f6a
guidolarosa <guido.glarosa@gmail.com>
2022-06-07 09:55:33 -0300

    Updated env

--- config/plugins.js
+++ config/plugins.js
@@ -10,9 +10,9 @@ module.exports = {
         config: {
           provider: 'cloudinary',
           providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_API_KEY'),
-            api_secret: env('CLOUDINARY_API_SECRET'),
+            cloud_name: env('CLOUDINARY_NAME', 'healight'),
+            api_key: env('CLOUDINARY_API_KEY', '385645811679574'),
+            api_secret: env('CLOUDINARY_API_SECRET', 'V8fxms3ZxN6HflANDBDfwAYSk20'),
           },
         //   actionOptions: {
         //     upload: {},
