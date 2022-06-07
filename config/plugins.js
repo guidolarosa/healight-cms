b0136f5604cc25b664ea6ce1261e73bf01f373e1
guidolarosa <guido.glarosa@gmail.com>
2022-06-07 09:45:38 -0300

    Added cloudinary plugin

--- config/plugins.js
+++ config/plugins.js
@@ -6,4 +6,19 @@ module.exports = {
             migrationFolderPath : 'migrations'
         },
     },
+    upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('healight'),
+            api_key: env('385645811679574'),
+            api_secret: env('V8fxms3ZxN6HflANDBDfwAYSk20'),
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+    },
 }
